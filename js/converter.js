a352e0da718ee17e2829a68ad998d2322244bb92
laddieri <laddieri@gmail.com>
2019-01-21 11:18:10 -0500

    Initial commit

---
+++ js/converter.js
@@ -0,0 +1,20 @@
+
+var outputText = document.getElementById('tempoText');
+var currentprice;
+
+let url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+
+fetch(url)
+.then(res => res.json())
+.then((out) => {
+  currentprice = parseInt(out.bpi.USD.rate.replace(/,/g,""),10);
+  console.log(currentprice);
+})
+.catch(err => { throw err });
+
+function convert(){
+  var satoshiInput = document.getElementById('satoshiInput').value;
+  var bitcoinOutput = satoshiInput * .00000001;
+  document.getElementById('outputText').textContent = bitcoinOutput + ' Bitcoin';
+  document.getElementById('ValueUSD').textContent = '$'+(bitcoinOutput * currentprice);
+}
